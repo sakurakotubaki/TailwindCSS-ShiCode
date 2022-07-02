b9c257ce96bcb516ec2ad1588d3da4b22e3dfc50
hashi <bluepatissere0310@iclud.com>
2022-07-02 10:34:43 +0900

    チュートリアルを終えた

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#eff8ff",
+          300: "390cdf4",
+          500: "#4299e1",
+        }
+      }
+    },
   },
   plugins: [],
 }
